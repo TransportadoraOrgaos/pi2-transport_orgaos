3b434aafb0a8b4102200876794f24d2f275270e4
Cristiano <engcristianoc@gmail.com>
2017-10-02 22:09:29 -0300

    Implementação inicial para login
    Signed-off-by: Cristiano <engcristianoc@gmail.com>

--- transportadora_orgaos/usuario/urls.py
+++ transportadora_orgaos/usuario/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^cadastro$', views.cadastro, name='cadastro')
+	url(r'^cadastro$', views.cadastro, name='cadastro'),
+	url(r'^login$', views.do_login, name='login'),
+	url(r'^logout$', views.do_logout, name='logout'),
 ]
--- transportadora_orgaos/usuario/views.py
+++ transportadora_orgaos/usuario/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from usuario.forms import UsuarioModelForm
 from django.views.generic import TemplateView
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 class Index(TemplateView):
     template_name = 'base.html'
 
+@login_required
 def cadastro(request):
 	form = UsuarioModelForm(request.POST or None)
 	context = {'form':form}
@@ -13,3 +16,15 @@ def cadastro(request):
 		if form.is_valid():
 			form.save()
 	return render(request, 'usuario/cadastro.html', context)
+
+def do_login(request):
+	if request.method == 'POST':
+		user = authenticate(username=request.POST['username'], password=request.POST['password'])
+		if user is not None:
+			login(request, user)
+			return redirect('/home')
+	return render(request, 'camaras_list.html')
+
+def do_logout(request):
+	logout(request)
+	return redirect('/login')
