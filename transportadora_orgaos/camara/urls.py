aa2633dea29df60a47ce3d8c78df2147302da5e0
Lucas Couto <loc.unb@gmail.com>
2017-10-06 20:42:37 -0300

    Adição de variáveis e início do camara_info
    - As variáveis responsible e organ foram adicionadas no projeto
    juntamente com os campos correspondentes no template de
    camara_cadastro.html
    
    - O método camara_info foi codificado em views, o seu template foi
    iniciado e uma rota foi adicionada para o método

--- transportadora_orgaos/camara/urls.py
+++ transportadora_orgaos/camara/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	 url(r'^camaras_list/$', views.camara_list, name='listar_camaras'),
-	 url(r'^camaras_cadastro/$', views.camara_cadastro, name='camaras_cadastro')
+	 url(r'^camaras_cadastro/$', views.camara_cadastro, name='camaras_cadastro'),
+	 url(r'^camara_info/(?P<camara_id>[0-9]+)$', views.camara_info, name='camara_info')
 ]
--- transportadora_orgaos/camara/views.py
+++ transportadora_orgaos/camara/views.py
@@ -7,7 +7,7 @@ import json
 class CamaraForm(ModelForm):
 	class Meta:
 		model = Camara
-		fields = ['name']
+		fields = ['name', 'responsible', 'organ']
 
 def camara_list(request, template_name='camaras_list.html'):
 	url = "https://transports-rest-api.herokuapp.com/transports"
@@ -21,15 +21,33 @@ def camara_list(request, template_name='camaras_list.html'):
 def camara_cadastro(request, template_name='camaras_cadastro.html'):
 	 
 	form = CamaraForm(request.POST or None)
-	payload = ""
+	
 	headers = {'content-type': 'application/json'}
 	
 	if form.is_valid():
+		responsible = form.cleaned_data['responsible']
+		organ = form.cleaned_data['organ']
+		payload = "{\n\t\"responsible\": \""+ responsible +"\",\n\t\"organ\": \""+ organ +"\"\n}"
 		url = "https://transports-rest-api.herokuapp.com/transport/" + form.cleaned_data['name']
 		response = requests.request("POST", url, data=payload, headers=headers)
+
 		if 'message' in response.json():
 			response_dict = response.json()
 			return render(request, template_name, {'form': form, 'response_dict': response_dict})
 		else:
 			return redirect('camara:listar_camaras')
 	return render(request, template_name, {'form': form})
+
+def camara_info(request, camara_id, template_name='camara_info.html'):
+	url = "https://transports-rest-api.herokuapp.com/report/" + str(camara_id)
+	headers = {
+		'content-type': 'application/json', 
+		'authorization': "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDczMDIxNzAs" +"Im5iZiI6MTUwNzMwMjE3MCwiaWRlbnRpdHkiOjEsImV4cCI6MTUwNzMwMjQ3MH0.8e5Dgf79L4bIUGHdwX"
+		+"--NgkSWzCz96UFuqvBKZ-jk3g"
+    }
+
+	camara_reports = requests.request("GET", url, headers=headers).json()
+
+	return render(request, template_name, {'camara_reports':camara_reports})
+
+	
