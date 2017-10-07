8433e7e6925e59d0a721c15d1548a5c32b66c651
Lucas Couto <loc.unb@gmail.com>
2017-10-07 00:40:32 -0300

    Primeira versão da página de informações da câmara

--- transportadora_orgaos/camara/views.py
+++ transportadora_orgaos/camara/views.py
@@ -42,11 +42,12 @@ def camara_info(request, camara_id, template_name='camara_info.html'):
 	url = "https://transports-rest-api.herokuapp.com/report/" + str(camara_id)
 	headers = {
 		'content-type': 'application/json', 
-		'authorization': "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDczMDIxNzAs" +"Im5iZiI6MTUwNzMwMjE3MCwiaWRlbnRpdHkiOjEsImV4cCI6MTUwNzMwMjQ3MH0.8e5Dgf79L4bIUGHdwX"
+		'authorization': "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDczMDIxNzAs" 
+		+"Im5iZiI6MTUwNzMwMjE3MCwiaWRlbnRpdHkiOjEsImV4cCI6MTUwNzMwMjQ3MH0.8e5Dgf79L4bIUGHdwX"
 		+"--NgkSWzCz96UFuqvBKZ-jk3g"
     }
 
-	camara_reports = requests.request("GET", url, headers=headers).json()
+	camara_reports = requests.request("GET", url, headers=headers).json()['reports']
 
 	return render(request, template_name, {'camara_reports':camara_reports})
 
