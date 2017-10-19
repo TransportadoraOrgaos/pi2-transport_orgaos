b8f147a79af2322e5907170feb69fcdad2a607a5
Anna Larissa <annalarissa.martins@hotmail.com>
2017-10-19 15:41:27 -0200

    Creating transport app

---
+++ transportadora_orgaos/transport/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+class Transport(models.Model):
+    organ = models.CharField(max_length = 50, null = False)
+    responsible = models.CharField(max_length = 50, null = False)
+    box_id = models.IntegerField(null = False)
---
+++ transportadora_orgaos/transport/views.py
@@ -0,0 +1,32 @@
+# -*-encoding: utf-8 -*-
+
+from django.forms import ModelForm
+from .models import *
+from django.shortcuts import render, redirect
+#from django.utils import simplejson
+import requests
+import json
+
+# Create your views here.
+class TransportForm(ModelForm):
+    class Meta():
+        model = Transport
+        fields = ['organ', 'responsible', 'box_id']
+
+def transport_cadastro(request, template_name='page_transport_cadastro.html'):
+    
+    form = TransportForm(request.POST or None)
+    if form.is_valid():
+        url = "https://transports-rest-api.herokuapp.com/transport/1"
+        payload = "{\n\t\"organ\": \"Kidnee\",\n\t\"responsible\": \"Bob\",\n\t\"box_id\": 1\n}"
+        headers = {'content-type': 'application/json'}
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+
+        if 'error_message' or 'message' in response.json():
+    			response_dict = response.json()
+			return render(request, template_name, {'form': form, 'response_dict': response_dict})
+        else:
+			return redirect('transport:listar_camaras')
+    else:
+        return render(request, template_name, {'form' : form})
