8b572f8d0180b78abd5c0199c56f976b63fd498f
Anna Larissa <annalarissa.martins@hotmail.com>
2017-10-19 23:03:06 -0200

    Getting transports list from api

--- transportadora_orgaos/transport/views.py
+++ transportadora_orgaos/transport/views.py
@@ -18,11 +18,11 @@ def transport_cadastro(request, box_id, template_name='page_transport_cadastro.h
     
     form = TransportForm(request.POST or None)
     if form.is_valid():
-        url = "https://transports-rest-api.herokuapp.com/transport/1"
+        url = "https://transports-rest-api.herokuapp.com/transport/1" + form.cleaned_data['organ'] + form.cleaned_data['responsible'] + form.cleaned_data['box_id']
         payload = "{\n\t\"organ\": \"Kidnee\",\n\t\"responsible\": \"Bob\",\n\t\"box_id\": 1\n}"
         headers = {'content-type': 'application/json'}
 
-        response = requests.request("POST", url, data=payload, headers=headers)
+        response = requests.post("POST", url, data=payload, headers=headers)
 
         if 'error_message' or 'message' in response.json():
     			response_dict = response.json()
@@ -32,7 +32,7 @@ def transport_cadastro(request, box_id, template_name='page_transport_cadastro.h
     else:
         return render(request, template_name, {'form' : form})
 
-def transport_list(request, template_name='page_transport_list.html'):   
+def transport_list(request, template_name='page_transports_list.html'):   
 
     url = "https://transports-rest-api.herokuapp.com/transports"
     headers = {'content-type': 'application/json'}
