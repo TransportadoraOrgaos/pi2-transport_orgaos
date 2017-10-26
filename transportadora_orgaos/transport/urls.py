6aa8fe622afeaef157b2c9c6fb03af698d6063f4
Lucas Couto <loc.unb@gmail.com>
2017-10-25 22:04:38 -0200

    Passando as info do ultimo report

--- transportadora_orgaos/transport/urls.py
+++ transportadora_orgaos/transport/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
 	 url(r'^transport_cadastro/(?P<box_id>[0-9]+)$', views.transport_cadastro, name='transport_cadastro'),
-	 url(r'^transport_list/$', views.transport_list, name='listar_transports'),
-	 url(r'^transport__info/(?P<box_id>[0-9]+)$', views.transport_info, name='transport_info')
+	 url(r'^transport__info/(?P<transport_id>[0-9]+)$', views.transport_info, name='transport_info')
 ]
--- transportadora_orgaos/transport/views.py
+++ transportadora_orgaos/transport/views.py
@@ -3,30 +3,29 @@
 from django.forms import ModelForm
 from .models import *
 from django.shortcuts import render, redirect
-#from django.utils import simplejson
 import requests
-import json
 
-# Create your views here.
+
 class TransportForm(ModelForm):
     class Meta():
         model = Transport
         fields = ['organ', 'responsible']
         
 def transport_cadastro(request, box_id, template_name='page_transport_cadastro.html'):
-    
     form = TransportForm(request.POST or None)
+
     if form.is_valid():
         transport_id = 1
         transport_id+=1
+
+        headers = {'content-type': 'application/json'}
         url = "https://transports-rest-api.herokuapp.com/transport/"+str(transport_id)
 
         organ = form.cleaned_data['organ']
         responsible = form.cleaned_data['responsible']
 
         payload = "{\n\t\"organ\": \"" + organ + "\",\n\t\"responsible\": \""+ responsible +"\",\n\t\"box_id\": "+ box_id +"\n}"
-        print(payload)
-        headers = {'content-type': 'application/json'}
+        
 
         response = requests.request("POST", url, data=payload, headers=headers)
 
@@ -34,26 +33,20 @@ def transport_cadastro(request, box_id, template_name='page_transport_cadastro.h
             response_dict = response.json()
             return render(request, template_name, {'form': form, 'response_dict': response_dict})
         else:
-            return redirect('transport:listar_camaras')
+            return redirect('camara:listar_camaras')
     return render(request, template_name, {'form' : form})
 
-def transport_list(request, template_name='page_transports_list.html'):   
 
-    url = "https://transports-rest-api.herokuapp.com/transports"
-    headers = {'content-type': 'application/json'}
-    transports = requests.request("GET", url, headers=headers)
 
-    transports_dict = transports.json()['transports']
+def transport_info(request, transport_id, template_name="page_reports.html"):
 
-    return render(request, template_name, {'transports_dict' : transports_dict})
-
-def transport_info(request, box_id, template_name="page_reports.html"):
-
-    url = "https://transports-rest-api.herokuapp.com/report/1"
+    #RECUPERAR REPORTS DO TRANSPORT_ID
+    url = "https://transports-rest-api.herokuapp.com/report/" + transport_id
     headers = {
         'content-type': "application/json",
         'authorization': "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDc5MTc0NzAsImlkZW50aXR5IjoyLCJleHAiOjE1MDc5MTc3NzAsIm5iZiI6MTUwNzkxNzQ3MH0.qsC3Md3L8Jc7WHfjqX5MpZYdtKbkJEiKT6ndpDFM7ho"
-        }
+    }
+
     transport_reports = requests.request("GET", url, headers=headers).json()['reports']
 
     temperaturas = []
@@ -61,7 +54,13 @@ def transport_info(request, box_id, template_name="page_reports.html"):
     for temperatura in transport_reports:
 		temperaturas.append([i, transport_reports[i]['temperature']])
 		i += 1
-        
-    print(temperaturas)
+
+    #RECUPERAR DADOS DO TRANSPORT_ID
+    url = "https://transports-rest-api.herokuapp.com/transport/" + transport_id
+    headers = {'content-type': 'application/json'}
+
+    transport = requests.request("GET", url, headers=headers).json()
+
+    #RECUPERAR DADOS DO BOX_ID
     
-    return render(request, template_name, {'transport_reports':transport_reports, 'temperaturas':temperaturas})
+    return render(request, template_name, {'transport_reports':transport_reports, 'transport':transport ,'temperaturas':temperaturas})
