9d373035e77d3dad8f4fe0994ccfd8fb1e3ff230
Cristiano <engcristianoc@gmail.com>
2017-10-26 23:31:48 -0200

    Controle de autorização, logout

--- transportadora_orgaos/usuario/urls.py
+++ transportadora_orgaos/usuario/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^cadastro$', views.cadastro, name='cadastro'),
 	url(r'^login$', views.do_login, name='login'),
-	# url(r'^logout$', views.do_logout, name='logout'),
+	url(r'^logout$', views.do_logout, name='logout'),
 	url(r'^list$', views.list, name='list'),
 ]
--- transportadora_orgaos/usuario/views.py
+++ transportadora_orgaos/usuario/views.py
@@ -10,25 +10,28 @@ class Index(TemplateView):
 
 
 def cadastro(request, template_name='usuario/cadastro.html'):
-	form = UsuarioModelForm(request.POST or None)
-	
-	if form.is_valid():
-		username = form.cleaned_data['username']
-		password = form.cleaned_data['password']
-		email = form.cleaned_data['email']
-		access_level = form.cleaned_data['access_level']
+	if 'token' in request.session:
+		form = UsuarioModelForm(request.POST or None)
+		
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			email = form.cleaned_data['email']
+			access_level = form.cleaned_data['access_level']
 
-		headers = {'content-type': 'application/json'}
-		payload = "{\n\t\"username\": \""+ username +"\",\n\t\"password\": \""+ password +"\", \n\t\"email\": \""+ email +"\", \n\t\"access_level\": \""+ access_level +"\"\n}"
-		url = "https://transports-rest-api.herokuapp.com/user"
-		response = requests.post(url, data=payload, headers=headers)
+			headers = {'content-type': 'application/json'}
+			payload = "{\n\t\"username\": \""+ username +"\",\n\t\"password\": \""+ password +"\", \n\t\"email\": \""+ email +"\", \n\t\"access_level\": \""+ access_level +"\"\n}"
+			url = "https://transports-rest-api.herokuapp.com/user"
+			response = requests.post(url, data=payload, headers=headers)
 
-		if 'error_message' or 'message' in response.json():
-			response_dict = response.json()
-			return render(request, template_name, {'form': form, 'response_dict': response_dict})
-		else:
-			return redirect('usuario:cadastro')
-	return render(request, template_name, {'form': form})
+			if 'error_message' or 'message' in response.json():
+				response_dict = response.json()
+				return render(request, template_name, {'form': form, 'response_dict': response_dict})
+			else:
+				return redirect('usuario:cadastro')
+		return render(request, template_name, {'form': form})
+	else:
+		return redirect('usuario:login')
 
 def do_login(request, template_name='usuario/login.html'):
 	form = UsuarioLoginForm(request.POST or None)
@@ -65,7 +68,7 @@ def do_logout(request):
 		del request.session['token']
 	except:
 		pass
-	return redirect('/login')
+	return redirect('usuario:login')
 
 def list(request, template_name='usuario/list.html'):
 	url = "https://transports-rest-api.herokuapp.com/users"
