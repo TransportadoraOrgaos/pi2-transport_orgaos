617c3c5a527607af36e404ede0c5f1c183c16cd2
Cristiano <engcristianoc@gmail.com>
2017-10-27 10:42:14 -0200

    Definindo autenticação para transports

--- transportadora_orgaos/transport/views.py
+++ transportadora_orgaos/transport/views.py
@@ -14,53 +14,58 @@ class TransportForm(ModelForm):
 def transport_cadastro(request, box_id, template_name='page_transport_cadastro.html'):
     form = TransportForm(request.POST or None)
 
-    if form.is_valid():
-        transport_id = 1
-        transport_id+=1
+    if 'token' in request.session:
+        if form.is_valid():
+            transport_id = 1
+            transport_id+=1
 
-        headers = {'content-type': 'application/json'}
-        url = "https://transports-rest-api.herokuapp.com/transport/"+str(transport_id)
+            headers = {'content-type': 'application/json'}
+            url = "https://transports-rest-api.herokuapp.com/transport/"+str(transport_id)
 
-        organ = form.cleaned_data['organ']
-        responsible = form.cleaned_data['responsible']
+            organ = form.cleaned_data['organ']
+            responsible = form.cleaned_data['responsible']
 
-        payload = "{\n\t\"organ\": \"" + organ + "\",\n\t\"responsible\": \""+ responsible +"\",\n\t\"box_id\": "+ box_id +"\n}"
-        
+            payload = "{\n\t\"organ\": \"" + organ + "\",\n\t\"responsible\": \""+ responsible +"\",\n\t\"box_id\": "+ box_id +"\n}"
+            
 
-        response = requests.request("POST", url, data=payload, headers=headers)
+            response = requests.request("POST", url, data=payload, headers=headers)
 
-        if 'error_message' or 'message' in response.json():
-            response_dict = response.json()
-            return render(request, template_name, {'form': form, 'response_dict': response_dict})
-        else:
-            return redirect('camara:listar_camaras')
-    return render(request, template_name, {'form' : form})
-
+            if 'error_message' or 'message' in response.json():
+                response_dict = response.json()
+                return render(request, template_name, {'form': form, 'response_dict': response_dict})
+            else:
+                return redirect('camara:listar_camaras')
+        return render(request, template_name, {'form' : form})
+    else:
+        return redirect('usuario:login')
 
 
 def transport_info(request, transport_id, template_name="page_reports.html"):
 
-    #RECUPERAR REPORTS DO TRANSPORT_ID
-    url = "https://transports-rest-api.herokuapp.com/report/" + transport_id
-    headers = {
-        'content-type': "application/json",
-        'authorization': "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDc5MTc0NzAsImlkZW50aXR5IjoyLCJleHAiOjE1MDc5MTc3NzAsIm5iZiI6MTUwNzkxNzQ3MH0.qsC3Md3L8Jc7WHfjqX5MpZYdtKbkJEiKT6ndpDFM7ho"
-    }
+    if 'token' in request.session:
+        #RECUPERAR REPORTS DO TRANSPORT_ID
+        url = "https://transports-rest-api.herokuapp.com/report/" + transport_id
+        headers = {
+            'content-type': "application/json",
+            'authorization': "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDc5MTc0NzAsImlkZW50aXR5IjoyLCJleHAiOjE1MDc5MTc3NzAsIm5iZiI6MTUwNzkxNzQ3MH0.qsC3Md3L8Jc7WHfjqX5MpZYdtKbkJEiKT6ndpDFM7ho"
+        }
 
-    transport_reports = requests.request("GET", url, headers=headers).json()['reports']
+        transport_reports = requests.request("GET", url, headers=headers).json()['reports']
 
-    temperaturas = []
-    i = 0
-    for temperatura in transport_reports:
-		temperaturas.append([i, transport_reports[i]['temperature']])
-		i += 1
+        temperaturas = []
+        i = 0
+        for temperatura in transport_reports:
+    		temperaturas.append([i, transport_reports[i]['temperature']])
+    		i += 1
 
-    #RECUPERAR DADOS DO TRANSPORT_ID
-    url = "https://transports-rest-api.herokuapp.com/transport/" + transport_id
-    headers = {'content-type': 'application/json'}
+        #RECUPERAR DADOS DO TRANSPORT_ID
+        url = "https://transports-rest-api.herokuapp.com/transport/" + transport_id
+        headers = {'content-type': 'application/json'}
 
-    transport = requests.request("GET", url, headers=headers).json()
+        transport = requests.request("GET", url, headers=headers).json()
 
-    #RECUPERAR DADOS DO BOX_ID
-    
-    return render(request, template_name, {'transport_reports':transport_reports, 'transport':transport ,'temperaturas':temperaturas})
+        #RECUPERAR DADOS DO BOX_ID
+        
+        return render(request, template_name, {'transport_reports':transport_reports, 'transport':transport ,'temperaturas':temperaturas})
+    else:
+        return redirect('usuario:login')
