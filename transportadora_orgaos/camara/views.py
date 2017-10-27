b5ec2dd13ce8c1634e7a18017066c7dbdc3bb76c
Cristiano <engcristianoc@gmail.com>
2017-10-27 10:39:04 -0200

    Definindo autenticação para as câmaras

--- transportadora_orgaos/camara/views.py
+++ transportadora_orgaos/camara/views.py
@@ -12,27 +12,33 @@ class CamaraForm(ModelForm):
 		fields = ['name']
 
 def camara_list(request, template_name='page_camaras_list.html'):
-	url = "https://transports-rest-api.herokuapp.com/boxes"
-	headers = {'content-type': 'application/json'}
+	if 'token' in request.session:
+		url = "https://transports-rest-api.herokuapp.com/boxes"
+		headers = {'content-type': 'application/json'}
 
-	camaras = requests.request("GET", url, headers=headers)
-	camaras_dict = camaras.json()['boxes']
-	
-	return render(request, template_name, {'camaras_dict':camaras_dict})
+		camaras = requests.request("GET", url, headers=headers)
+		camaras_dict = camaras.json()['boxes']
+		
+		return render(request, template_name, {'camaras_dict':camaras_dict})
+	else:
+		return redirect('usuario:login')
 
 def camara_cadastro(request, template_name='page_camara_cadastro.html'):
 	form = CamaraForm(request.POST or None)
 	
-	if form.is_valid():
-		headers = {'content-type': 'application/json'}
-		payload = ""
-		url = "https://transports-rest-api.herokuapp.com/box/" + form.cleaned_data['name']
-		response = requests.post(url, data=payload, headers=headers)
-		import ipdb; ipdb.set_trace()
+	if 'token' in request.session:
+		if form.is_valid():
+			headers = {'content-type': 'application/json'}
+			payload = ""
+			url = "https://transports-rest-api.herokuapp.com/box/" + form.cleaned_data['name']
+			response = requests.post(url, data=payload, headers=headers)
+			import ipdb; ipdb.set_trace()
 
-		if 'error_message' or 'message' in response.json():
-			response_dict = response.json()
-			return render(request, template_name, {'form': form, 'response_dict': response_dict})
-		else:
-			return redirect('camara:listar_camaras')
-	return render(request, template_name, {'form': form})
+			if 'error_message' or 'message' in response.json():
+				response_dict = response.json()
+				return render(request, template_name, {'form': form, 'response_dict': response_dict})
+			else:
+				return redirect('camara:listar_camaras')
+		return render(request, template_name, {'form': form})
+	else:
+		return redirect('usuario:login')
