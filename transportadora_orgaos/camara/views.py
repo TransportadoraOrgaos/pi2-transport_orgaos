fd71ea64ebb1dfe5c04acd6dd27f826349714dd3
Anna Larissa <annalarissa.martins@hotmail.com>
2017-10-31 23:31:42 -0200

    Getting transports list

--- transportadora_orgaos/camara/views.py
+++ transportadora_orgaos/camara/views.py
@@ -40,21 +40,17 @@ def camara_list(request, template_name='page_camaras_list.html'):
 
 
 def camara_delete(request, camara_name, template_name="page_camaras_list.html"):
-	
-	if form.is_valid():
-    		headers = {'content-type': 'application/json'}
-		payload = ""
-		url = "https://transports-rest-api.herokuapp.com/box/" + form.cleaned_data['name']
-		response = requests.post(url, data=payload, headers=headers)
+    	
+	payload = ""
+	headers = {'content-type': 'application/json'}
 
-		if 'error_message' or 'message' in response.json():
-			response_dict = response.json()
-			return render(request, template_name, {'form': form, 'response_dict': response_dict})
-		else:
-			return redirect('camara:listar_camaras')
-	return render(request, template_name, {'form': form})
+	#Deletar câmara específica
+	url = "https://transports-rest-api.herokuapp.com/box/" + camara_name
+	response = requests.request("DELETE", url, data=payload, headers=headers)
 
-def get_all_reports(request, template_name='camaras_list_for_reports.html'):
+	return redirect('camara:listar_camaras')
+
+def get_all_boxes(request, template_name='all_camaras_reports.html'):
     	url = "https://transports-rest-api.herokuapp.com/boxes"
         headers = {'content-type': 'application/json'}
         
@@ -62,9 +58,26 @@ def get_all_reports(request, template_name='camaras_list_for_reports.html'):
         camaras_dict = camaras.json()['boxes']
         return render(request, template_name, {'camaras_dict':camaras_dict})
 
-def get_transports_from_box(request, camara_name, template_name="transports_list_for_report.html"):
+def get_transports_from_box(request, camara_name, template_name="transports_list_for_reports.html"):
 		url = "https://transports-rest-api.herokuapp.com/box/" + camara_name
 		headers = {'content-type': 'application/json'}
 		camara_transports = requests.request("GET", url, headers=headers).json()['transports']
 
 		return render(request, template_name, {'camara_transports' : camara_transports})
+
+def generate_pdf(request):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(0,0, "Hello world.")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
