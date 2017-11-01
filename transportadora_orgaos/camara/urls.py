d50b005047307474c391044fc5c368879f0f24b1
Anna Larissa <annalarissa.martins@hotmail.com>
2017-11-01 18:54:36 -0200

    creating general pdf

--- transportadora_orgaos/camara/urls.py
+++ transportadora_orgaos/camara/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	 url(r'^camaras_list/$', views.camara_list, name='listar_camaras'),
+	 url(r'^generate_pdf/$', views.generate_pdf, name='generate_pdf'),
 	 url(r'^camaras_list_for_reports/$', views.get_all_boxes, name='listar_camaras_relatorios'),
 	  url(r'^all_reports_list/$', views.get_all_boxes, name='listar_camaras_completas'),
 	 url(r'^transport_list_for_reports/(?P<camara_name>.*)$', views.get_transports_from_box, name='transport_list_reports'),
--- transportadora_orgaos/camara/views.py
+++ transportadora_orgaos/camara/views.py
@@ -62,3 +62,20 @@ def reports_list(request, camara_name, transport_id, template_name="reports_list
 
 	print(response.text)
 	return render(request, template_name, {'reports_dict':reports_dict})
+
+def generate_pdf(request):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(0,0, "Hello world.")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
