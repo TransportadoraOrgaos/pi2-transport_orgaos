18d9a794d80d2cde3b463dc89abb02d60f05c8ce
Lucas Couto <loc.unb@gmail.com>
2017-11-13 14:44:22 -0200

    Alterações para rodar o pytest

--- transport/tests.py
+++ transport/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from transportadora_orgaos.settings import INSTALLED_APPS
 
 # Create your tests here.
+
+class SettingsTest(TestCase):    
+    def test_camara_is_configured(self):
+        assert 'camara' in INSTALLED_APPS
