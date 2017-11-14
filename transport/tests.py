0c157f4983b4274fcb7d9ebc31cfeb0bbd6a2e53
Lucas Couto <loc.unb@gmail.com>
2017-11-13 23:12:17 -0200

    Criando um teste para passar a build e testando o deploy no heroku

--- transport/tests.py
+++ transport/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SettingsTest(TestCase):    
+    def test_camara_is_configured(self):
+        assert 'camara' in INSTALLED_APPS
--- transportadora_orgaos/settings.py
+++ transportadora_orgaos/settings.py
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'transportadora_orgaos.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'transorg'
     }
 }
 
