d997901b0902c59e19de023d2572c24c8cb41137
Cristiano <engcristianoc@gmail.com>
2017-11-23 21:29:46 -0200

    Modificando estrutura testes

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 class PagesTest(TestCase):
+
+    def setUp(self):
+        self.url_login = reverse('usuario:login')
+        self.url_cadastro = reverse('usuario:cadastro')
+
+
     def test_login(self):
-        response = self.client.get('/login')
+        response = self.client.get(self.url_login)
         self.assertEqual(response.status_code, 200)
 
     def test_cadastro(self):
-        response = self.client.get('/cadastro')
+        response = self.client.get(self.url_cadastro)
         self.assertEqual(response.status_code, 302)
