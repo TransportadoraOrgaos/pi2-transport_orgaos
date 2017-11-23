08eba2a10786eb1767d8101e9dbc66453eb6bbec
Cristiano <engcristianoc@gmail.com>
2017-11-23 20:14:43 -0200

    Teste para requisições de páginas

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PagesTest(TestCase):
+    def test_login(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+    def test_cadastro(self):
+        response = self.client.get('/cadastro')
+        self.assertEqual(response.status_code, 302)
