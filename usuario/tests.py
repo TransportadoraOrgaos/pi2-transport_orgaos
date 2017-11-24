16aa7d99a9c0a0ff19bf2e6fa840d6b65b23f704
Lucas Couto <loc.unb@gmail.com>
2017-11-24 01:29:50 -0200

    usuario.tests atualizado

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
-import django
-django.setup()
+import transportadora_orgaos.settings
 
 class PagesTest(TestCase):
 
