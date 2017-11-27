09859267baecfcbfa9fc90e817c91ce4a9fe5dda
Anna Larissa <annalarissa.martins@hotmail.com>
2017-11-27 15:24:07 -0200

    Adding camara tests

--- camara/tests.py
+++ camara/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class CamaraTest(TestCase):
+
+    def setUp(self):
+        self.url_cadastro = reverse('camara:camaras_cadastro')
+        self.url_list = reverse('camara:listar_camaras')
+        self.url_all_boxes = reverse('camara:relatorio_geral')
+
+    def test_cadastro(self):
+        response = self.client.get(self.url_cadastro)
+        self.assertEqual(response.status_code, 302)
+
+    def test_list(self):
+        response = self.client.get(self.url_list)
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_all_boxes(self):
+        response = self.client.get(self.url_all_boxes)
+        self.assertEqual(response.status_code, 302)
+        
--- camara/views.py
+++ camara/views.py
@@ -83,24 +83,6 @@ def get_transports_from_box(request, camara_name, template_name="transports_list
     else:
         return redirect('usuario:login')
 
-# def generate_pdf(request):
-#     # Create the HttpResponse object with the appropriate PDF headers.
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
-
-#     # Create the PDF object, using the response object as its "file."
-#     p = canvas.Canvas(response)
-
-#     # Draw things on the PDF. Here's where the PDF generation happens.
-#     # See the ReportLab documentation for the full list of functionality.
-#     p.drawString(0,0, "Hello world.")
-
-#     # Close the PDF object cleanly, and we're done.
-#     p.showPage()
-#     p.save()
-#     return response
-
-
 def generate_pdf(request):
     url = "https://transports-rest-api.herokuapp.com/boxes"
     headers = {'content-type': 'application/json', 'authorization': 'jwt ' + request.session['token']['access_token']}
