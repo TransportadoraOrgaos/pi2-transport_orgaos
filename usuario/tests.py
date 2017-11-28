2a42b429f7277bbecc2c100bc9c5da7b5ab67af8
Cristiano <engcristianoc@gmail.com>
2017-11-28 16:56:07 -0200

    Teste para del ususario

--- usuario/tests.py
+++ usuario/tests.py
@@ -9,7 +9,7 @@ class PagesTest(TestCase):
         self.url_cadastro = reverse('usuario:cadastro')
         self.url_denied = reverse('usuario:denied')
         self.url_list = reverse('usuario:list')
-        self.url_del_user = reverse('usuario:list')
+        self.url_del_user = reverse('usuario:user_delete', kwargs={'users_username': 'aaaa'})
 
         self.headers = {'content-type': 'application/json'}
         self.username = 'teste'
@@ -20,15 +20,36 @@ class PagesTest(TestCase):
         self.token = requests.post(self.url, data=self.payload, headers=self.headers).json()
 
 
+    def test_cadastro(self):
+        response = self.client.get(self.url_cadastro)
+        self.assertEqual(response.status_code, 302)
 
+    def test_cadastro_user(self):
+    	session = self.client.session
+        session['token'] = self.token
+        session['username'] = self.username
+        session.save()
+
+        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
+
+        response = self.client.post(self.url_cadastro, form_data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_cadastro_sem_access_level(self):
+    	session = self.client.session
+        session['token'] = self.token
+        session['username'] = self.username_usuario
+        session.save()
+
+        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
+
+        response = self.client.post(self.url_cadastro, form_data)
+        self.assertEqual(response.status_code, 302)
 
     def test_login(self):
         response = self.client.get(self.url_login)
         self.assertEqual(response.status_code, 200)
 
-    def test_cadastro(self):
-        response = self.client.get(self.url_cadastro)
-        self.assertEqual(response.status_code, 302)
 
     def test_denied(self):
     	response = self.client.get(self.url_denied)
@@ -56,42 +77,9 @@ class PagesTest(TestCase):
     	response = self.client.get(self.url_list)
         self.assertEqual(response.status_code, 302)
 
-    def test_cadastro_user(self):
-    	session = self.client.session
-        session['token'] = self.token
-        session['username'] = self.username
-        session.save()
-
-        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
-
-        response = self.client.post(self.url_cadastro, form_data)
-        self.assertEqual(response.status_code, 200)
-
-    def test_cadastro_sem_access_level(self):
-    	session = self.client.session
-        session['token'] = self.token
-        session['username'] = self.username_usuario
-        session.save()
-
-        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
-
-        response = self.client.post(self.url_cadastro, form_data)
-        self.assertEqual(response.status_code, 302)
-
-    def test_del_user(self):
-    	session = self.client.session
-        session['token'] = self.token
-        session['username'] = self.username
-        session.save()
-
-        form_data = {'username': 'aaaa'}
-
-        response = self.client.delete(self.url_del_user, form_data)
-
-        self.assertEqual(response.status_code, 200)
 
     def test_login_user(self):
-    	form_data = {'username': 'aaaa','password': '12345'}
+    	form_data = {'username': 'teste','password': 'teste'}
         
         response = self.client.post(self.url_login, form_data)
         
@@ -103,3 +91,44 @@ class PagesTest(TestCase):
         response = self.client.post(self.url_login, form_data)
         
         self.assertEqual(response.status_code, 200)
+
+    def test_del_user(self):
+    	session = self.client.session
+        session['token'] = self.token
+        session['username'] = self.username
+        session.save()
+
+        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
+
+        response = self.client.post(self.url_cadastro, form_data)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.delete(self.url_del_user)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_del_user_sem_token(self):
+    	
+        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
+
+        response = self.client.post(self.url_cadastro, form_data)
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.delete(self.url_del_user)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_del_user_sem_access_level(self):
+    	session = self.client.session
+        session['token'] = self.token
+        session['username'] = self.username_usuario
+        session.save()
+
+        form_data = {'username': 'aaaa','password': '12345', 'email': 'aaa@aa.com', 'access_level': 'Administrador'}
+
+        response = self.client.post(self.url_cadastro, form_data)
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.delete(self.url_del_user)
+
+        self.assertEqual(response.status_code, 302)
